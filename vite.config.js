99433e046b65ac30f7b1654a43cd95a775a063b8
osqtr <clecat1005@gmail.com>
2025-11-22 06:11:33 +0900

    vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      srcDir: "src",
+      filename: "service-worker.js",
+      strategies: "generateSW",
+
+      manifest: {
+        name: "OurReading",
+        short_name: "OurReading",
+        theme_color: "#46cdcf",
+        icons: [{ src: "/logo.svg", sizes: "any", type: "image/svg+xml" }],
+      },
+
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,json}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
