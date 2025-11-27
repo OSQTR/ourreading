80a3487a3a0a931a5077ba7595525260c3de5b32
osqtr <clecat1005@gmail.com>
2025-11-27 12:15:22 +0900

    remove index.css, global styled

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./store/store.js";
-import "./index.css";
 import GlobalStyles from "./styles/GlobalStyles.js";
 
 createRoot(document.getElementById("root")).render(
